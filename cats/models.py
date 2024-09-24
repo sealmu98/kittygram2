9cf3ccd18260095ea27dfea8bb506b9f9539b41e
sealmu <seal.mu98@gmail.com>
2024-09-25 00:17:36 +0300

    Unique

--- cats/models.py
+++ cats/models.py
@@ -27,6 +27,17 @@ class Cat(models.Model):
         User, related_name='cats', on_delete=models.CASCADE)
     achievements = models.ManyToManyField(Achievement, through='AchievementCat')
 
+    # class Meta:
+    #     unique_together = ('name', 'owner')
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['name', 'owner'],
+                name='unique_name_owner'
+            )
+        ]
+
     def __str__(self):
         return self.name
 
